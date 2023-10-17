546e50a23a685aefb5ede296059aa28af2bd29f2
lagovic <lagovic@gmail.com>
2023-10-17 16:41:19 -0700

    precision.c

---
+++ precision.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * get_precision - Calculating printing precision
+ * @format: Format to print arguments
+ * @i: Argument list to print
+ * @list: List of arguments.
+ * Return: Precision if present
+ */
+int get_precision(const char *format, int *i, va_list list)
+{
+	int c = *i + 1;
+	int precision = -1;
+
+	if (format[c] != '.')
+		return (precision);
+	precision = 0;
+
+	for (c += 1; format[c] != '\0'; c++)
+	{
+		if (is_digit(format[c]))
+		{
+			precision *= 10;
+			precision += format[c] - '0';
+		}
+		else if (format[c] == '*')
+		{
+			c++;
+			precision = va_arg(list, int);
+			break;
+		}
+		else
+			break;
+	}
+
+	*i = c - 1;
+
+	return (precision);
+}
