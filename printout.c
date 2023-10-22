fe00a319c527b6f997f9df958751e7aa9aa6ba86
lagovic <lagovic@gmail.com>
2023-10-22 12:44:48 -0700

    printo,unsigned,util

---
+++ printout.c
@@ -0,0 +1,49 @@
+#include "main.h"
+/**
+ * handle_print - Prints arguments based on type
+ * @fmt: Formatted string in which to print the arguments.
+ * @list: Argument list to print.
+ * @ind: ind.
+ * @buffer: Buffer array handling print.
+ * @flags: Gets active flags
+ * @width: To get width
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: 1 or 2;
+ */
+int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
+                int flags, int width, int precision, int size)
+{
+        int v, unknow_len = 0, printed_chars = -1;
+        fmt_t fmt_types[] = {
+                {'c', print_char}, {'s', print_string}, {'%', print_percent},
+                {'i', print_int}, {'d', print_int}, {'b', print_binary},
+                {'u', print_unsigned}, {'o', print_octal}, {'x', print_hexadecimal},
+                {'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
+                {'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
+        };
+        for (v = 0; fmt_types[v].fmt != '\0'; v++)
+                if (fmt[*ind] == fmt_types[v].fmt)
+                        return (fmt_types[v].fn(list, buffer, flags, width, precision, size));
+
+        if (fmt_types[v].fmt == '\0')
+        {
+                if (fmt[*ind] == '\0')
+                        return (-1);
+                unknow_len += write(1, "%%", 1);
+                if (fmt[*ind - 1] == ' ')
+                        unknow_len += write(1, " ", 1);
+                else if (width)
+                {
+                        --(*ind);
+                        while (fmt[*ind] != ' ' && fmt[*ind] != '%')
+                                --(*ind);
+                        if (fmt[*ind] == ' ')
+                                --(*ind);
+                        return (1);
+			}
+                unknow_len += write(1, &fmt[*ind], 1);
+                return (unknow_len);
+        }
+        return (printed_chars);
+}
---
+++ util.c
@@ -0,0 +1,82 @@
+#include "main.h"
+/**
+ * is_printable - function that checks if a char is printable
+ * @c: Character to be evaluated.
+ *
+ * Return: 1 if c is printable, 0 otherwise
+ */
+int is_printable(char c)
+{
+        if (c >= 32 && c < 127)
+                return (1);
+
+        return (0);
+}
+
+/**
+ * append_hexa_code - Attach ascci in hexadecimal code to buffer
+ * @buffer: Array of chars.
+ * @i: Index at which to start appending.
+ * @ascii_code: ASSCI CODE.
+ * Return: Always 3
+ */
+int append_hexa_code(char ascii_code, char buffer[], int i)
+{
+        char map_to[] = "0123456789ABCDEF";
+
+        if (ascii_code < 0)
+                ascii_code *= -1;
+
+        buffer[i++] = '\\';
+        buffer[i++] = 'x';
+
+        buffer[i++] = map_to[ascii_code / 16];
+        buffer[i] = map_to[ascii_code % 16];
+
+        return (3);
+}
+
+/**
+ * is_digit - confirms if c is a digit
+ * @c: Char to be evaluated
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+int is_digit(char c)
+{
+        if (c >= '0' && c <= '9')
+                return (1);
+
+        return (0);
+}
+
+/**
+ * convert_size_number - Displays a number to the specified size
+ * @num: Number to be displayed.
+ * @size: Number indicating the type to be displayed.
+ * Return: Displayed value of number displayed
+ */
+long int convert_size_number(long int num, int size)
+{
+        if (size == S_LONG)
+                return (num);
+        else if (size == S_SHORT)
+                return ((short)num);
+
+        return ((int)num);
+}
+
+/**
+ * convert_size_unsgnd - Displays a number to specified size
+ * @num: Displayed number
+ * @size: Number indicating the type to be displayed
+ * Return: Displayed value of num
+ */
+long int convert_size_unsgnd(unsigned long int num, int size)
+{
+        if (size == S_LONG)
+                return (num);
+        else if (size == S_SHORT)
+                return ((unsigned short)num);
+
+        return ((unsigned int)num);
+}
